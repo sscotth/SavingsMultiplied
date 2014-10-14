c972d579a13e61780e363ee6a11a68e538898094
Scott <sscotth@gmail.com>
2014-10-14 00:27:35 -0500

    move javascript to its own file

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+  var loginModal = document.querySelector('.login-modal');
+  var lightbox = document.querySelector('.lightbox');
+  var loginLink = document.querySelector('.login');
+  var cancelButton = document.querySelector('input[value="Cancel"]');
+
+  lightbox.addEventListener('click', function(){
+    hide(loginModal);
+  });
+
+  cancelButton.addEventListener('click', function(){
+    hide(loginModal);
+  });
+
+  loginLink.addEventListener('click', function(){
+    show(loginModal);
+  });
+});
+
+function hide(element){
+  element.style.display = 'none';
+}
+
+function show(element){
+  element.style.display = 'block';
+}
